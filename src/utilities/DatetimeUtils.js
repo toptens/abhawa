47d31cabbb646f5a960e9120b78e9d1dde8c1fab
TopTens <91243892+toptens@users.noreply.github.com>
2023-11-28 22:52:20 +0600

    Update DatetimeUtils.js

--- src/utilities/DatetimeUtils.js
+++ src/utilities/DatetimeUtils.js
@@ -18,14 +18,14 @@ export function getDayMonthFromDate() {
 }
 
 export function transformDateFormat() {
-  const month = date.toLocaleString('en-US', { month: '2-digit' });
-  const day = date.toLocaleString('en-US', { day: '2-digit' });
+  const month = date.toLocaleString('bn-BD', { month: '2-digit' });
+  const day = date.toLocaleString('bn-BD', { day: '2-digit' });
   const year = date.getFullYear();
-  const time = date.toLocaleString('en-US', {
+  const time = date.toLocaleString('bn-BD', {
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit',
-    hourCycle: 'h23',
+    hourCycle: 'h12',
   });
 
   const newFormatDate = year.toString().concat('-', month, '-', day, ' ', time);
@@ -33,11 +33,11 @@ export function transformDateFormat() {
 }
 
 export function getUTCDatetime() {
-  const utcTime = date.toLocaleString('en-US', {
+  const utcTime = date.toLocaleString('bn-BD', {
     hour: '2-digit',
     minute: '2-digit',
     hourCycle: 'h23',
-    timeZone: 'UTC',
+    timeZone: 'UTC+6',
   });
 
   const isoDateString = new Date().toISOString();
@@ -46,12 +46,12 @@ export function getUTCDatetime() {
 }
 
 export function getUTCTime() {
-  const utcTime = date.toLocaleString('en-US', {
+  const utcTime = date.toLocaleString('bn-BD', {
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit',
-    hourCycle: 'h23',
-    timeZone: 'UTC',
+    hourCycle: 'h12',
+    timeZone: 'UTC+6',
   });
 
   return utcTime;
