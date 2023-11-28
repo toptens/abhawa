a610cb17e4615e1b803ff12955a5b8fddae41286
TopTens <91243892+toptens@users.noreply.github.com>
2023-11-28 22:40:57 +0600

    Update ApiService.js

--- src/utilities/ApiService.js
+++ src/utilities/ApiService.js
@@ -1,6 +1,6 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'YOUR API KEY';
+const WEATHER_API_KEY = '5c9a2c84e9cf63916e9568582333124e';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
